097359879e19728fbb0ff9931f5c70743f544ed9
LiuHaoLung <louis17171717@gmail.com>
2020-03-13 16:31:31 +0800

    SQL Code

--- SQLCode/PrimaryKeyConstraint.sql
+++ SQLCode/PrimaryKeyConstraint.sql
@@ -1,6 +1,5 @@
 DROP TABLE sales;
 
-
 CREATE TABLE sales
 (
 	purchase_number INT AUTO_INCREMENT,
@@ -8,4 +7,31 @@ CREATE TABLE sales
     customer_id INT,
     item_code VARCHAR(10) NOT NULL,
     PRIMARY KEY (purchase_number)
+);
+
+CREATE TABLE customers
+(
+	customer_id INT,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email_address VARCHAR(255),
+    number_of_complaints INT,
+    PRIMARY KEY(customer_id)
+);
+
+CREATE TABLE items
+(
+	item_code VARCHAR(255),
+    item VARCHAR(255),
+    unit_price numeric(10,2),
+    company_id VARCHAR(255),
+    PRIMARY KEY(item_code)
+);
+
+CREATE TABLE companies
+(
+	company_id VARCHAR(255),
+    company_name VARCHAR(255),
+    headquarters_phone_number int(12),
+    PRIMARY KEY(company_id)
 );
