f180609a09cac6b1cb67aa857ac8f440e390e8e6
LiuHaoLung <louis17171717@gmail.com>
2020-03-16 15:11:39 +0800

    Select

---
+++ SQLCode/Select/And.sql
@@ -0,0 +1,17 @@
+/*
+the technical term for the = sign is equal operator
+in SQL, there are many other linking and symbols, called operators, that you can use with the WHERE clause
+like AND, OR, IN, NOT IN, LIKE, NOT LIKE, BETWEEN ... AND...
+EXISTS, NOT EXISTS, IS NULL, IS NOT NULL, comparison operators and so on
+AND is an operator that allows you to logically combine two statements in the condition code block
+for instance, it can retrieve data about all employees whose first_name is Denis and who are male
+so the AND operator allows us to narrow the output we would like to extract from the data
+technically this operator must be placed right after the first condition
+*/
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Denis' AND GENDER = 'M';
