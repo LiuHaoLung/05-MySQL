7c060f8de69efe2b7f24a3e9d209395d47a95609
LiuHaoLung <louis17171717@gmail.com>
2020-03-18 14:47:00 +0800

    Aggregate Functions

---
+++ SQLCode/AggregateFunctions/MinandMax.sql
@@ -0,0 +1,21 @@
+# MAX and MIN returns the max or min value of a column
+
+SELECT 
+    MAX(salary)
+FROM
+    salaries;
+    
+SELECT 
+    MIN(salary)
+FROM
+    salaries;
+
+SELECT 
+    MAX(emp_no)
+FROM
+    employees;
+    
+SELECT 
+    MIN(emp_no)
+FROM
+    employees;
